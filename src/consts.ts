7b519779dc83a1c87e4e9081f9798abbe053dcfa
Andrey <codenomnom@users.noreply.github.com>
2025-01-09 15:01:51 +0200

    feat: theme switcher
    + using css variables across the scss file

--- src/consts.ts
+++ src/consts.ts
@@ -1,6 +1,3 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
 export const SiteTitle = 'prev·log';
 export const SiteDescription = 'Welcome to my website!';
 export const SiteBase = '/prev-log';
