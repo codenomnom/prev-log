7bf31d1696f682e1ded3e9f30792e52249c29977
Andrey <codenomnom@users.noreply.github.com>
2025-01-10 16:22:56 +0200

    feat: open external links in new window (rehype plugin)

--- astro.config.ts
+++ astro.config.ts
@@ -1,9 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import rehypeExternalLinks from 'rehype-external-links';
 import { SiteBase } from './src/consts';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://codenomnom.github.io',
   base: SiteBase,
+  markdown: {
+    rehypePlugins: [[rehypeExternalLinks, { target: '_blank' }]],
+  },
 });
