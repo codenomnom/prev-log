4c2c7e0cdc141a744e776a55bb5dea192a094143
Andrey <codenomnom@users.noreply.github.com>
2025-01-28 11:01:46 +0200

    chore: x og

--- src/consts.ts
+++ src/consts.ts
@@ -1,3 +1,4 @@
 export const SiteTitle = 'prev·log';
 export const SiteDescription = 'a log of headbangs, when Next resulted in prev';
 export const SiteBase = '';
+export const TwitterHandle = 'prev_log';
